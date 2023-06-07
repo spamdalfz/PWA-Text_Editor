76d49891223cc4dc62275c0d74e9471f546c901b
spamdalfz <coreyvasser@gmail.com>
2023-06-07 16:16:03 -0700

    new file:   .gitignore new file:   client/.gitignore new file:   client/package-lock.json modified:   client/src/js/database.js new file:   package-lock.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Adds the given content to the database
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  tx.store.put({ content });
+  await tx.done;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Retrieves all the content from the database
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const content = await tx.store.getAll();
+  await tx.done;
+  return content;
+};
 
 initdb();
